84b8685d2c682c0df4d8619ac31af5475b3e9588
NickKirby20 <kirbynic000@warren.k12.in.us>
2021-11-29 12:35:32 -0500

    Quiz Game and more done

--- L2-If Else Statements/src/ShippingCosts.java
+++ L2-If Else Statements/src/ShippingCosts.java
@@ -1,63 +1,62 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import jdk.nashorn.internal.scripts.JO;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class ShippingCosts {
 
     /*
     A Shipping Company charges the following rates
     for each 100 miles shipped:
-
     2lbs or less ..................... $1.10
     over 2lbs but less than 6lbs ..... $2.20
     over 6lbs but less than 10lbs .... $3.70
     over 10lbs ....................... $4.80
-
     Write a program to calculate the shipping price. The
     distance is not prorated, in other words, the price doesn't
     increase until the next 100 miles, i.e. 150 miles for a 3lbs
     package is still just $2.20.
-
     Example Run:
     Weight: 5.6lbs
     Distance: 345 miles
     Price: $6.60
-
     As always, your program should include a method.
      */
 
     public static void main(String[] args) {
 
-        double itemWeight = Double.parseDouble(JOptionPane.showInputDialog("How much is the weight"));
-        int milesShipped = Integer.parseInt(JOptionPane.showInputDialog("How many miles will it ship"));
+        double itemWeight = Double.parseDouble(JOptionPane.showInputDialog("How much os the weight?"));
+        int milesShipped = Integer.parseInt(JOptionPane.showInputDialog("How many miles will it ship?"));
         double shippingRate;
         double cost;
 
-        if (itemWeight >= 10){
+        if(itemWeight>= 10){
             shippingRate = 4.80;
         }else{
-            if (itemWeight <= 6){
+            if(itemWeight >= 6){
                 shippingRate = 3.70;
             }else{
-                if (itemWeight >= 2){
+                if(itemWeight >= 2){
                     shippingRate = 2.20;
                 }else{
                     shippingRate = 1.10;
+                }
             }
+        }
 
-                cost = cost(shippingRate, milesShipped);
-                JOptionPane.showMessageDialog(null,"The cost is" + cost);
+        cost = cost(shippingRate, milesShipped);
+
+        JOptionPane.showMessageDialog(null, " The cost is " + cost);
 
     }
-
     public static double cost(double rate, int miles){
-
         int costMultiplier = miles/100;
 
-        if (costMultiplier == 0){
+        if(costMultiplier == 0){
             costMultiplier = 1;
         }
 
         return rate * costMultiplier;
-
     }
-
 }
--- L3-Random Numbers/src/QuizGame.java
+++ L3-Random Numbers/src/QuizGame.java
@@ -1,2 +1,205 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+
 public class QuizGame {
+    static double userAnswer, correctAnswer;
+    static String displayProblem = "";
+
+   static JPanel panel;
+    static JButton createProblem, checkAnswer;
+    static JLabel showProblem, resultLabel;
+    static JTextField answerInput;
+    static JRadioButton addition, subtraction, multiplication, division;
+    static ButtonGroup radios = new ButtonGroup();
+
+    public static void main(String[] args) {
+
+        new QuizGame();
+        /*int choice =Integer.parseInt(JOptionPane.showInputDialog( " Choose Addition(1) , Subtraction(2), Multiplication(3), Division(4)");
+
+        createProblem(choice);
+        getUserAnswer();
+        checkAnswer();*/
+
+
+    }
+
+    public QuizGame(){
+
+        JFrame frame = new JFrame("Quiz Game");
+        frame.setSize(300, 450);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        panel =new JPanel();
+
+        addition = new JRadioButton("Addition");
+        subtraction = new JRadioButton("Subtraction");
+        multiplication = new JRadioButton("Multiplication");
+        division = new JRadioButton("Division");
+        createProblem = new JButton("Make A problem");
+        showProblem = new JLabel("PLACE HOLDER");
+        answerInput = new JTextField(10);
+        checkAnswer = new JButton("CHECK ANSWER");
+        resultLabel = new JLabel("Answer Result");
+
+        addition.setBounds(100, 25, 200 ,20);
+        subtraction.setBounds(100, 50, 200, 20);
+        multiplication.setBounds(100, 75, 200, 20);
+        division.setBounds(100, 100, 200, 20);
+
+        createProblem.setBounds(50, 125, 200, 25);
+        showProblem.setBounds(50, 150, 200, 25);
+        answerInput.setBounds(50, 175, 200, 25);
+        checkAnswer.setBounds(50, 200, 200, 25);
+        resultLabel.setBounds(50, 225, 200, 25);
+
+        addition.setSelected(true);
+
+        createProblem.addActionListener(new NewProblemButton());
+        checkAnswer.addActionListener(new checkProblemListener());
+
+        radios.add(addition);
+        radios.add(subtraction);
+        radios.add(multiplication);
+        radios.add(division);
+
+
+        panel.setLayout(null);
+        panel.add(addition);
+        panel.add(subtraction);
+        panel.add(multiplication);
+        panel.add(division);
+        panel.add(createProblem);
+        panel.add(showProblem);
+        panel.add(answerInput);
+        panel.add(checkAnswer);
+        panel.add(resultLabel);
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+    }
+
+    private class NewProblemButton implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            if (addition.isSelected()){
+                add();
+            }
+            if (subtraction.isSelected()){
+                sub();
+            }
+            if (multiplication.isSelected()){
+                mult();
+            }
+            if (division.isSelected()){
+                div();
+            }
+
+            showProblem.setText(displayProblem);
+            panel.remove(createProblem);
+            panel.updateUI();
+        }
+    }
+
+    private class checkProblemListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            getUserAnswer();
+            checkAnswer();
+        }
+    }
+    public static void checkAnswer(){
+        if (userAnswer == correctAnswer){
+            resultLabel.setText("you are correct");
+            panel.add(createProblem);
+            panel.updateUI();
+        }else{
+            resultLabel.setText( "Incorrect, pleas tey again ");
+        }
+    }
+
+    public static void getUserAnswer(){
+
+        userAnswer = Double.parseDouble(answerInput.getText());
+
+    }
+
+    public static void createProblem(int choice)   {
+
+
+        if(choice >= 1){;
+            if(choice<5){
+                if(choice ==1){
+                    add();
+                }
+                if (choice == 2){
+                    sub();
+                }
+                if(choice == 3){
+                    mult();
+                }
+                if(choice==4){
+                    div();
+                }
+            }else {System.out.println("Wrong input, restart program, try again");
+                System.exit(0);
+            }
+
+        }else{
+            System.out.println("Wrong input, restart program, try again");
+            System.exit(0);
+        }
+
+
+    }
+
+    public static void add(){
+        int num1 = random();
+        int num2 = random();
+
+        correctAnswer = num1 + num2;
+        displayProblem = num1 + " + " + num2;
+    }
+
+    public static void sub(){
+        int num1 = random();
+        int num2 = random();
+
+        correctAnswer = num1 - num2;
+        displayProblem = num1 + " - " + num2;
+    }
+
+    public static void mult(){
+        int num1 = random();
+        int num2 = random();
+
+        correctAnswer = num1 * num2;
+        displayProblem = num1 + " X " + num2;
+    }
+
+    public static void div(){
+        int num1 = random();
+        int num2 = random();
+
+        correctAnswer = (num1 / num2);
+
+        correctAnswer = Math.round(correctAnswer * 100.0)/100.0;
+
+        displayProblem = num1 + " / " + num2;
+    }
+    //HELPER METHOD
+    public static int random() {
+        Random random = new Random();
+        return random.nextInt(20)+1;
+    }
+
+    public static double input(String message){
+      return Double.parseDouble(JOptionPane.showInputDialog(message));
+    }
+
 }
